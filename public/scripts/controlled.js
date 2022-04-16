947c823a8b47a2aec47c905126ac7b8a785bc672
bacnk <nguyenkhacbac1233@gmail.com>
2022-04-16 09:56:47 +0700

    Update controlled.js

--- public/scripts/controlled.js
+++ public/scripts/controlled.js
@@ -1,15 +1,15 @@
 var led_one = document.getElementById('onBtn');
 var led_off = document.getElementById('offBtn');
-
+var firebaseRef = firebase.database().ref().child("LedStatus");
  function onFunction() {
-    State.set('ON')
+    firebaseRef.set('ON')
     console.log("0n")
     OUT_TEXT.innerText = 'Chế độ tự động';
 }
 
   
 function offFunction() {
-    State.set('OFF')
+    firebaseRef.set('OFF')
     OUT_TEXT.innerText = 'Chế độ bằng tay';
     console.log("0ff")
 }
